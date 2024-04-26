8b98df148898704c480c864b38511d6e4514e1d0
unknown <swarna.restu@gmail.com>
2024-04-26 18:27:08 +0700

    update parser ultrasonic SWM

--- modules/parser/dj-ultrasonic-water-meter.js
+++ modules/parser/dj-ultrasonic-water-meter.js
@@ -58,29 +58,21 @@ class DjUltrasonicWaterMeter {
             {
                 let meterReading = payload.substring(12, 19)
                 meterReading = meterReading.match(/[a-fA-F0-9]{2}/g).reverse().join('')
-                //meterReading = parseInt(meterReading)
+                meterReading = parseInt(meterReading)
                 message.meterReading = meterReading/1000
-                console.log(message.meterReading)
-
                 let valveStatus = payload.substring(20, 21)
                 if (valveStatus == "00")
                 {
                     message.valveStatus = "1"
                 }
-                else if ( valveStatus == "01")
+                else
                 {
                     message.valveStatus = "0"
                 }
-                else 
-                {
-                    message.valveStatus = "2"
-                }
-
-                console.log(valveStatus)
                 let battery = payload.substring(24, 25)
                 battery = parseInt(battery)
                 message.battery = (battery/255)*100
-                console.log(Battery)                
+                console.log(battery)                
             }
 
             console.log(chalk.white("writing new data"))
